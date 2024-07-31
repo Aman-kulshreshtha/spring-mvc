37c636032b071568870f8482d607c0464abfa8ec
amankulshreshtha <aman21102000@gmail.com>
2024-07-31 16:13:42 +0530

    passing data from controller to view using Model

--- src/main/java/com/example/mvc/controller/HelloController.java
+++ src/main/java/com/example/mvc/controller/HelloController.java
@@ -2,13 +2,15 @@ package com.example.mvc.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String getHelloPage() {
+    public String getHelloPage(Model model) {
+        model.addAttribute("name", new String("Chaman"));
         return "hello";
     }
 }
--- src/main/webapp/WEB-INF/views/hello.jsp
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -1,6 +1,7 @@
 <html>
 <body>
 <h2>Hello World!</h2>
-This is from hello.jsp
+This is from hello.jsp<br/>
+So Your name is <%=request.getAttribute("name")%>
 </body>
 </html>
